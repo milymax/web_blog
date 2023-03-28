141545846b63df470732f87c5369a0412f765e66
milymax <vararakya@gmail.com>
2023-03-28 13:14:15 +0700

    redesign ui

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,11 +2,17 @@
 
 @section('container')
 
-        <h2 class="mb-5">{{ $post->title }}</h2>
-
-        <p>By. <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name}}</a></p>
-
-        {!! $post->body !!}
-
-    <a href="/posts" class="d-block mt-3">Back to Post</a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 ">
+                <h1 class="mb-5">{{ $post->title }}</h1>
+            
+                <p>By. <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name}}</a></p>
+            
+                {!! $post->body !!}
+            
+            <a href="/posts" class="d-block mt-3">Back to Post</a>
+        </div>
+    </div>
+</div>
 @endsection
