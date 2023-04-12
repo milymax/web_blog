9a2fa8c0145e5f1bc67ec4ac03711212e77867ac
milymax <vararakya@gmail.com>
2023-04-13 01:50:00 +0700

    fix fitur registrasi user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Database\Query\IndexHint;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -18,13 +20,27 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|max:255', //tulis dengan tanda pipe
             'username' => ['required', 'min:3', 'max:255', 'unique:users'], //ataupun array sama saja
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:6|max:255'
         ]);
+        
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']); //dua dua nya sama untuk mengamankan password
+        
+        //membuat user baru dari variable validatedData yang dikirim ke database Users
+        User::create($validatedData);
 
-        dd('registrasi berhasil!!!');
+        // $request->session()->flash('successRegister', 'Registration Successfull, Login');
+
+        // $this->$request->session()->flash('status', 'Task was successful!');
+        
+
+        //redirect ke halaman login dengan membawa pesan flash dibawah ini
+        return redirect('/login')->with('successRegister', 'Registration successfull, login now');
+          
+        // dd('registrasi berhasil!!!');
     }
 }
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -9,7 +9,12 @@
                 
                 <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
               <form>
-            
+                @if (session()->has('successRegister'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                  {{ session('successRegister') }}
+                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                @endif
                 <div class="form-floating">
                   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                   <label for="floatingInput">Email address</label>
